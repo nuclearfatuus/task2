37f11fad0f0e512cb9193779bb065c5013e92a8b
tolyone <tolybook@gmaill.com>
2018-11-15 13:28:55 +0300

    third task

--- main.cpp
+++ main.cpp
@@ -1,36 +1,16 @@
 #include <iostream>
 
-void f2(float k){
-    int a = k/1.0;
-
-    float b = k - a;
-
-    float c = b*100;
-
-    int d = c/1.0;
-
-    float e = c - d;
-
-    if (e>0){
-        std::cout << "k has 3 or more digits after comma" << std::endl;
-    }
-    else {
-        std::cout << "k has less than 3 digits after comma" << std::endl;
-    }
-}
 
 void f1(float k){
-    if(k>=-100 && k<=37.5) {
-            std::cout << "k is belong to [-100;37.5]" << std::endl;
-            f2(k);
-        }
-        else {
-        std::cout << "k is not belong to [-100;37.5]" << std::endl;
-        }
+    float a = 10.0;
+   while ((k/a)> 0.1 || (k/a)<-0.1) {
+       std::cout << "1" << std::endl;
+       a *= 10;
+   }
 }
 
 int main() {
-    float k = -152.49;
+    float k = 100.49;
     f1(k);
     return 0;
 }
