cf8b80b70d31814e8a52f8b51cd26b4c711e15ac
tolyone <tolybook@gmaill.com>
2018-11-15 13:36:48 +0300

    fifth task

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 
 
-void f1(float k){
-    float a = 10.0;
-   while ((k/a)> 0.1 || (k/a)<-0.1) {
-       std::cout << "1" << std::endl;
-       a *= 10;
-   }
+void f1(int a, float k){
+    switch(a){
+        case 1: std::cout << "Hello, me name is f1" << std::endl;
+        break;
+        case 2: {int b = k/1.0;
+            std::cout << b << std::endl;}
+            break;
+        case 3: {int b = k/1.0;
+        float c = k-b;
+            std::cout << c << std::endl;}
+            break;
+        default: std::cout << "Hello, my name is f1" << std::endl;
+    }
 }
 
 int main() {
+    int a = 5;
     float k = 100.49;
-    f1(k);
+    f1(a, k);
     return 0;
 }
